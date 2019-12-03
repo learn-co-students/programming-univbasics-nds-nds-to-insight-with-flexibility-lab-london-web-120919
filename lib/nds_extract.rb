901194b561267dcaf11762744dcf6f7ed5ddd87a
James Inglis <jinglis12@googlemail.com>
2019-12-03 15:49:01 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -48,6 +48,15 @@ def movies_with_director_key(name, movies_collection)
   # Array of Hashes where each Hash represents a movie; however, they should all have a
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
+  
+  i = 0
+  movies_array = []
+  
+  while i < movies_collection.length
+    movies_array.push(movie_with_director_name(name, movies_collection[i]))
+    i += 1
+  end
+  movies_array
 end
 
 
@@ -63,6 +72,19 @@ def gross_per_studio(collection)
   #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
+  
+  i = 0
+  studios_gross_total = {}
+  
+  while i < collection.length
+    if studios_gross_total[collection[i][:studio]]
+      studios_gross_total[collection[i][:studio]] += collection[i][:worldwide_gross]
+    else 
+      studios_gross_total[collection[i][:studio]] = collection[i][:worldwide_gross]
+    end
+    i += 1
+  end
+  studios_gross_total
 end
 
 def movies_with_directors_set(source)
@@ -76,6 +98,15 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
+
+  i = 0
+  directors_movies_array = []
+  
+  while i < source.length
+    directors_movies_array.push(movies_with_director_key(source[i][:name], source[i][:movies]))
+    i += 1
+  end
+  directors_movies_array
 end
 
 # ----------------    End of Your Code Region --------------------
